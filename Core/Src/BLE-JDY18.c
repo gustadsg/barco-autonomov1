8d7a96320c1ebf7740a356e941b91abc099f6094
João Prates <jvdprates@hotmail.com>
2023-11-05 21:29:04 -0300

    refactor: Formating

--- Core/Src/BLE-JDY18.c
+++ Core/Src/BLE-JDY18.c
@@ -6,34 +6,27 @@
  */
 #include "BLE-JDY18.h"
 
-UART_HandleTypeDef* husart;
+UART_HandleTypeDef *husart;
 
-const char *JDY18_CommandPrefixStrings[] = {
-	//@brief JDY18_Command_SetConfig,
-    "PERM",
-	//@brief JDY18_Command_Reset,
-    "DEFAULT",
-	//@brief JDY18_Command_SetName,
-	"AT+NAME",
-	//@brief JDY18_Command_GetName,
-    "NAME",
-	//@brief JDY18_Command_SetRole,
-    "ROLE",
-	//@brief JDY18_Command_SetBaudRate,
-    "BAUD",
-	//@brief JDY18_Command_Scan
-    "IQN"
-};
+const char *JDY18_CommandPrefixStrings[] = { "PERM", // JDY18_Command_SetConfig
+		"DEFAULT", // JDY18_Command_Reset
+		"AT+NAME", // JDY18_Command_SetName
+		"NAME", // JDY18_Command_GetName
+		"ROLE", // JDY18_Command_SetRole
+		"BAUD", // JDY18_Command_SetBaudRate
+		"IQN" // JDY18_Command_Scan
+		};
 
-void JDY18_Setup(UART_HandleTypeDef* handle) {
+void JDY18_Setup(UART_HandleTypeDef *handle) {
 	husart = handle;
 }
 
-void JDY18_SendCommand(JDY18_CommandPrefix_t commandIndex, char* commandParam) {
-	const char* commandPrefix = JDY18_CommandPrefixStrings[commandIndex];
-	char* command = "AT+";
+void JDY18_SendCommand(JDY18_CommandPrefix_t commandIndex, char *commandParam) {
+	const char *commandPrefix = JDY18_CommandPrefixStrings[commandIndex];
+	char *command = "AT+";
 
-	int commandSize = strlen(command) + strlen(commandPrefix) + strlen(commandParam) + 3;
+	int commandSize = strlen(command) + strlen(commandPrefix)
+			+ strlen(commandParam) + 3;
 	command = malloc(commandSize);
 	memset(command, 0, commandSize);
 
@@ -45,7 +38,7 @@ void JDY18_SendCommand(JDY18_CommandPrefix_t commandIndex, char* commandParam) {
 	HAL_UART_Transmit(husart, (uint8_t*) command, commandSize, HAL_MAX_DELAY);
 }
 
-void JDY18_GetName(char* name) {
+void JDY18_GetName(char *name) {
 	name = malloc(24);
 	memset(name, 0, 24);
 
@@ -65,7 +58,6 @@ void JDY18_SetBaudRate(JDY18_BaudRate_t baud) {
 	char baudStr[5] = "";
 	itoa(baud, baudStr, 10);
 
-
 	JDY18_SendCommand(JDY18_Command_SetBaudRate, baudStr);
 }
 
@@ -76,17 +68,20 @@ void JDY18_Scan(JDY18_Device_t *devices) {
 	JDY18_SendCommand(JDY18_Command_Scan, "");
 	HAL_UART_Receive(husart, (uint8_t*) data, JDY18_BUFFER_SZ, 1000);
 
-	int numDevices = __JDY18_GetDevicesFromScanStr(data, devices, JDY18_MAX_DEVICES);
+	int numDevices = __JDY18_GetDevicesFromScanStr(data, devices,
+	JDY18_MAX_DEVICES);
 	__JDY18_GetDistanceFromRssi(devices, numDevices);
 }
 
 void __JDY18_GetDistanceFromRssi(JDY18_Device_t *devices, int8_t numDevices) {
-	for(int i=0; i<numDevices; i++) {
-		devices[i].distance = pow(10, (JDY18_DEFAULT_POWER-devices[i].rssi)/(JDY18_N*10));
+	for (int i = 0; i < numDevices; i++) {
+		devices[i].distance = pow(10,
+				(JDY18_DEFAULT_POWER - devices[i].rssi) / (JDY18_N * 10));
 	}
 }
 
-int __JDY18_GetDevicesFromScanStr(char* str, JDY18_Device_t *devices, int8_t maxDevices) {
+int __JDY18_GetDevicesFromScanStr(char *str, JDY18_Device_t *devices,
+		int8_t maxDevices) {
 	char inputCopy[JDY18_BUFFER_SZ];
 	strcpy(inputCopy, str);
 
@@ -99,7 +94,7 @@ int __JDY18_GetDevicesFromScanStr(char* str, JDY18_Device_t *devices, int8_t max
 			int id, rssi;
 			char name[18];
 
-			char* deviceInfoRegex = "+DEV:%d=%*[^,],%d,%18[^\\r\\n]";
+			char *deviceInfoRegex = "+DEV:%d=%*[^,],%d,%18[^\\r\\n]";
 
 			if (sscanf(token, deviceInfoRegex, &id, &rssi, name) == 3) {
 				if (deviceCount < maxDevices) {
