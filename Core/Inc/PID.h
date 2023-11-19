c04390cddc464b3341667c2734528b338f5bd6f4
João Prates <jvdprates@hotmail.com>
2023-11-19 19:39:59 -0300

    feat: Improving PID lib with comments

--- Core/Inc/PID.h
+++ Core/Inc/PID.h
@@ -12,35 +12,86 @@
 #define PID_LAST 1
 
 typedef struct {
+	// Proportional gain
 	float Kp;
+
+	// Integral gain
 	float Ki;
+
+	// Derivative gain
 	float Kd;
 
+	// Setpoint: the desired value the system should reach
 	float setpoint;
+
+	// Measured value: the current value of the system
 	float measured;
 
+	// Maximum output value of the controller
 	float maxOutput;
+
+	// Minimum output value of the controller
 	float minOutput;
 
-	float errorArr[2];
-	float errorAcc;
-	float errorDer;
+	// Array to store the last two errors for derivative calculation
+	float errorArray[2];
 
+	// Integral of the error over time
+	float errorIntegral;
+
+	// Derivative of the error with respect to time
+	float errorDerivative;
+
+	// The period of time in milliseconds at which the controller operates
 	int periodMs;
 } PID_Controller_t;
 
-void PID_Create(PID_Controller_t *controller, float kp, float ki, float kd, int periodMs);
+/**
+ * @brief Initializes a PID controller with the specified gains and control period.
+ * @param controller Pointer to the PID controller structure to be initialized.
+ * @param kp Proportional gain.
+ * @param ki Integral gain.
+ * @param kd Derivative gain.
+ * @param periodMs Time period in milliseconds at which the controller operates.
+ */
+void PID_Create(PID_Controller_t *controller, float kp, float ki, float kd,
+                int periodMs);
 
+/**
+ * @brief Sets the saturation limits for the PID controller's output.
+ * @param controller Pointer to the PID controller structure.
+ * @param min Minimum allowable output value.
+ * @param max Maximum allowable output value.
+ */
 void PID_SetSaturationLimits(PID_Controller_t *controller, float min, float max);
 
+/**
+ * @brief Sets the desired setpoint for the PID controller.
+ * @param controller Pointer to the PID controller structure.
+ * @param setpoint The desired setpoint value the system should reach.
+ */
 void PID_SetSetpoint(PID_Controller_t *controller, float setpoint);
 
+/**
+ * @brief Processes a new input value for the PID controller.
+ * @param controller Pointer to the PID controller structure.
+ * @param input The current input value to the system.
+ */
 void PID_ProcessInput(PID_Controller_t *controller, float input);
 
+/**
+ * @brief Calculates the control action based on the PID algorithm.
+ * @param controller Pointer to the PID controller structure.
+ * @return The calculated control action.
+ */
 float PID_CalculateControlAction(PID_Controller_t *controller);
 
+/**
+ * @brief Applies saturation limits to the original control output.
+ * @param controller Pointer to the PID controller structure.
+ * @param originalOutput The control output before saturation.
+ * @return The saturated control output within the specified limits.
+ */
 float __PID_SaturateOutput(PID_Controller_t *controller, float originalOutput);
-
-
 
 #endif /* INC_PID_H_ */
--- Core/Src/PID.c
+++ Core/Src/PID.c
@@ -16,11 +16,11 @@ void PID_Create(PID_Controller_t *controller, float kp, float ki, float kd, int 
 	controller->setpoint = 0;
 	controller->measured = 0;
 
-	controller->errorAcc = 0;
-	controller->errorDer = 0;
+	controller->errorIntegral = 0;
+	controller->errorDerivative = 0;
 
-	controller->errorArr[PID_CURRENT] = 0;
-	controller->errorArr[PID_LAST] = 0;
+	controller->errorArray[PID_CURRENT] = 0;
+	controller->errorArray[PID_LAST] = 0;
 
 	controller->periodMs = periodMs;
 
@@ -39,20 +39,20 @@ void PID_SetSetpoint(PID_Controller_t *controller, float setpoint) {
 }
 
 void PID_ProcessInput(PID_Controller_t *controller, float input) {
-	float error = setpoint-input;
+	float error = controller->setpoint-input;
 
-	controller->errorAcc += error*controller->periodMs;
+	controller->errorIntegral += error*controller->periodMs;
 
-	controller->errorArr[PID_LAST] = controller->errorArr[PID_CURRENT];
-	controller->errorArr[PID_CURRENT] = error;
+	controller->errorArray[PID_LAST] = controller->errorArray[PID_CURRENT];
+	controller->errorArray[PID_CURRENT] = error;
 
-	controller->errorDer = (controller->error[PID_CURRENT] - controller->errorArr[PID_LAST])/controller->periodMS;
+	controller->errorDerivative = (controller->errorArray[PID_CURRENT] - controller->errorArray[PID_LAST])/controller->periodMs;
 }
 
 float PID_CalculateControlAction(PID_Controller_t *controller) {
-	float P = controller->Kp*errorArr[PID_CURRENT];
-	float I = controller->Ki*errorAcc;
-	float D = controller->Kd*errorDer;
+	float P = controller->Kp*controller->errorArray[PID_CURRENT];
+	float I = controller->Ki*controller->errorIntegral;
+	float D = controller->Kd*controller->errorDerivative;
 
 	float PID = __PID_SaturateOutput(controller, P+I+D);
 
@@ -61,7 +61,7 @@ float PID_CalculateControlAction(PID_Controller_t *controller) {
 
 float __PID_SaturateOutput(PID_Controller_t *controller, float originalOutput) {
 	// limit integral error
-	if(controller->errorAcc > controller->maxOutput) controller->errorAcc = controller->maxOutput;
+	if(controller->errorIntegral > controller->maxOutput) controller->errorIntegral = controller->maxOutput;
 
 	if(originalOutput > controller->maxOutput) return controller->maxOutput;
 	if(originalOutput < controller->minOutput) return controller->minOutput;
